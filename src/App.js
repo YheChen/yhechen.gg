0e6fa9df41d9ce9adc9f923f6c4705d8df14098b
YheChen <yanzhenchen123@gmail.com>
2024-02-29 20:11:18 -0500

    Deleted unneeded files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [playerData, setPlayerData] = useState({});
+  const [additionalPlayerData, setAdditionalPlayerData] = useState({});
+  const API_KEY = "RGAPI-71450655-539c-4edc-85c6-2b021fb6b865";
+
+  function searchForPlayer(event) {
+    const [searchUsername, searchTagline] = searchText.split("#");
+    var APICallString = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${searchUsername}/${searchTagline}?api_key=${API_KEY}`;
+
+    axios
+      .get(APICallString)
+      .then(function (response) {
+        setPlayerData(response.data);
+
+        var puuid = response.data.puuid;
+        var APICallString1 = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${API_KEY}`;
+
+        return axios.get(APICallString1);
+      })
+      .then(function (response) {
+        setAdditionalPlayerData(response.data);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h5>League Player Finder</h5>
+        <input
+          type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button onClick={(e) => searchForPlayer(e)}>Search for Player</button>
+      </div>
+      {JSON.stringify(additionalPlayerData) !== "{}" ? (
+        <>
+          <p>
+            {additionalPlayerData.gameName} + {additionalPlayerData.tagLine}
+          </p>
+          <p>Summoner Level {additionalPlayerData.summonerLevel}</p>
+        </>
+      ) : (
+        <>
+          <p>No Player Data</p>
+        </>
+      )}
     </div>
   );
 }
