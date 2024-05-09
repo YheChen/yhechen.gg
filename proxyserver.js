af92b38b68aef79ca696ac211dcdca16a7b1e788
YheChen <yanzhenchen123@gmail.com>
2024-05-09 16:50:44 -0400

    ???

--- proxyserver.js
+++ proxyserver.js
@@ -8,6 +8,26 @@ app.use(cors());
 
 const API_KEY = "RGAPI-ec025171-908f-40bc-b3fb-e48936aff41e";
 
+function findPlayerPUUID(searchUsername, searchTagline) {
+  const riotAPICallString = `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${searchUsername}/${searchTagline}?api_key=${API_KEY}`;
+  axios.get(riotAPICallString).then(function (response) {
+    const PUUID = response.data.puuid;
+  });
+  return PUUID;
+}
+
+app.get("/leagueV4", async (req, res) => {
+  const playerName = req.query.username;
+  const playerTag = req.query.tagline;
+
+  const playerPUUID = await findPlayerPUUID(playerName, playerTag);
+  const leagueV4apiCall = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${playerPUUID}?api_key=${API_KEY}`;
+
+  const leagueV4data = await axios
+    .get(leagueV4apiCall)
+    .then((response) => response.data);
+});
+
 app.listen(4000, function () {
   console.log("server started at port 4000");
 });
